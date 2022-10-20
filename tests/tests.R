2ffcd089b7428c53d00229ebb469833372ed9aea
Yihui Xie <xie@yihui.name>
2022-10-20 11:05:03 -0500

    merge markdownExtensions.R into render-options.R

--- tests/tests.R
+++ tests/tests.R
@@ -1,6 +1,5 @@
 library(markdown)
 
-source(system.file('examples', 'markdownExtensions.R', package = 'markdown'), echo = TRUE)
 source(system.file('examples', 'render-options.R', package = 'markdown'), echo = TRUE)
 
 rm(mkd)
