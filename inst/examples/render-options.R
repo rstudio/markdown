0ca3352afbdef4f2f6e58ad885980976531db8b3
Yihui Xie <xie@yihui.name>
2022-10-23 22:10:36 -0500

    use renderMarkdown() instead of markdownToHTML() so that we don't need to set the `fragment_only` option

--- inst/examples/render-options.R
+++ inst/examples/render-options.R
@@ -1,17 +1,14 @@
 library(markdown)
 
-# generate HTML fragments only
-options(markdown.html.options = "fragment_only")
-
 # toc example
 mkd <- c("# Header 1", "p1", "## Header 2", "p2")
 
-cat(markdownToHTML(text = mkd))
-cat(markdownToHTML(text = mkd, options = "toc"))
+cat(renderMarkdown(text = mkd))
+cat(renderMarkdown(text = mkd, options = "toc"))
 
 # hard_wrap example
-cat(markdownToHTML(text = "foo\nbar\n"))
-cat(markdownToHTML(text = "foo\nbar\n", options = "hard_wrap"))
+cat(renderMarkdown(text = "foo\nbar\n"))
+cat(renderMarkdown(text = "foo\nbar\n", options = "hard_wrap"))
 
 # latex math example
 mkd <- c(
@@ -22,22 +19,22 @@ a^{2}+b^{2} & = & c^{2}\\\\
 \\end{eqnarray}"
 )
 
-cat(markdownToHTML(text = mkd))
-cat(markdownToHTML(text = mkd, options = "-latex_math"))
+cat(renderMarkdown(text = mkd))
+cat(renderMarkdown(text = mkd, options = "-latex_math"))
 
 # smartypants example
-cat(markdownToHTML(text = "1/2 (c)"))
-cat(markdownToHTML(text = "1/2 (c)", options = "-smartypants"))
+cat(renderMarkdown(text = "1/2 (c)"))
+cat(renderMarkdown(text = "1/2 (c)", options = "-smartypants"))
 
 mkd <- names(markdown:::pants)
 mkd <- paste(c(mkd, paste0('`', mkd, '`')), collapse = ' ')
-cat(markdownToHTML(text = mkd))
-cat(markdownToHTML(text = mkd, options = "-smartypants"))
+cat(renderMarkdown(text = mkd))
+cat(renderMarkdown(text = mkd, options = "-smartypants"))
 
 cat(smartypants("1/2 (c)\n"))
 
 # tables example (need 4 spaces at beginning of line here)
-cat(markdownToHTML(text = "
+cat(renderMarkdown(text = "
 First Header  | Second Header
 ------------- | -------------
 Content Cell  | Content Cell
@@ -45,7 +42,7 @@ Content Cell  | Content Cell
 "))
 
 # but not here
-cat(markdownToHTML(text = "
+cat(renderMarkdown(text = "
 First Header  | Second Header
 ------------- | -------------
 Content Cell  | Content Cell
@@ -53,23 +50,21 @@ Content Cell  | Content Cell
 ", options = '-table'))
 
 # autolink example
-cat(markdownToHTML(text = "https://www.r-project.org/"))
-cat(markdownToHTML(text = "https://www.r-project.org/", options = "-autolink"))
+cat(renderMarkdown(text = "https://www.r-project.org/"))
+cat(renderMarkdown(text = "https://www.r-project.org/", options = "-autolink"))
 
 # strikethrough example
-cat(markdownToHTML(text = "~~awesome~~"))
-cat(markdownToHTML(text = "~~awesome~~", options = "-strikethrough"))
+cat(renderMarkdown(text = "~~awesome~~"))
+cat(renderMarkdown(text = "~~awesome~~", options = "-strikethrough"))
 
 # superscript and subscript examples
-cat(markdownToHTML(text = "2^10^"))
-cat(markdownToHTML(text = "2^10^", options = "-superscript"))
-cat(markdownToHTML(text = "H~2~O"))
-cat(markdownToHTML(text = "H~2~O", options = "-subscript"))
+cat(renderMarkdown(text = "2^10^"))
+cat(renderMarkdown(text = "2^10^", options = "-superscript"))
+cat(renderMarkdown(text = "H~2~O"))
+cat(renderMarkdown(text = "H~2~O", options = "-subscript"))
 
 # skip_html tags
 mkd = '<style>a {}</style><script type="text/javascript">console.log("No!");</script>\n[Hello](#)'
-cat(markdownToHTML(text = mkd))
+cat(renderMarkdown(text = mkd))
 # TODO: wait for https://github.com/r-lib/commonmark/issues/15 to be fixed
-# cat(markdownToHTML(text = mkd, options = "tagfilter"))
-
-options(markdown.html.options = NULL)
+# cat(renderMarkdown(text = mkd, options = "tagfilter"))
