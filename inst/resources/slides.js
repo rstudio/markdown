cfb0a46e09729096c2df28b07a284dc863530698
Yihui Xie <xie@yihui.name>
2023-01-10 00:24:42 -0600

    JS code to wrap each slide in <div class="slide">, and implement keyboard shortcuts (currently `f` and `o`)

---
+++ inst/resources/slides.js
@@ -0,0 +1,71 @@
+(function(d) {
+  const n = 3; // find a container that has at least n "slides"
+  let p = d.body;  // container of slides; assume <body> for now
+  // add 'slide' class to the frontmatter div and toc
+  ['.frontmatter', '#TOC'].forEach((s) => {
+    const fm = p.querySelector(s);
+    fm && fm.classList.add('slide');
+  });
+  const s1 = ':scope > hr:not([class])', s2 = ':scope > h2';
+  function findContainer(s) {
+    if (p.querySelectorAll(s).length >= n) return true;
+    // if body doesn't contain headers or <hr>s, look into children
+    for (let i = 0; i < p.children.length; i++) {
+      if (p.children[i].querySelectorAll(s).length >= n) {
+        p = p.children[i];
+        break;
+      }
+    }
+    return false;
+  }
+  if (!findContainer(s1)) {
+    // if not enough <hr>s found in children; look for <h2> instead
+    if (p.tagName === 'BODY') {
+      // not enough h2 found, this page is not appropriate for slides
+      if (!findContainer(s2) && p.tagName === 'BODY') return;
+      const h2s = p.querySelectorAll(s2);
+      for (let i = 0; i < h2s.length; i++) {
+        h2s[i].before(d.createElement('hr'));
+      }
+    }
+  }
+
+  function newSlide() {
+    let s = d.createElement('div')
+    s.className = 'slide';
+    return s;
+  }
+  function isSep(el) {
+    return el.tagName === 'HR' && el.attributes.length === 0;
+  }
+  let el = p.firstElementChild;
+  if (isSep(el)) el.remove();
+  el = p.firstElementChild;
+  if (!el) return;
+  let s = newSlide();
+  el.before(s);
+  while (true) {
+    let el = s.nextElementSibling;
+    if (!el) break;
+    // remove slide separators (<hr>) and create new slide
+    if (isSep(el)) {
+      s = newSlide();
+      el.before(s);
+      el.remove();
+    } else if (el.classList.contains('slide')) {
+      if (s.innerText === '') {
+        el.after(s);
+      } else {
+        s = newSlide();
+        el.after(s);
+      }
+    } else {
+      s.append(el);
+    }
+  }
+  // press f for fullscreen mode
+  document.addEventListener('keyup', (e) => {
+    e.key === 'f' && d.documentElement.requestFullscreen();
+    e.key === 'o' && d.body.classList.toggle('overview');
+  });
+})(document);
