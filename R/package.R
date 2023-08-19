1d8291ed92656db3f5221f6d2873a311d79be96e
Yihui Xie <xie@yihui.name>
2023-08-19 09:08:55 -0500

    add the alias `markdown-package` to the package Rd via the roxygen2 sentinel `_PACKAGE` per request from CRAN: https://github.com/r-lib/roxygen2/issues/1491

--- R/package.R
+++ R/package.R
@@ -3,7 +3,4 @@
 #' \pkg{Markdown} is a plain-text formatting syntax that can be converted to
 #' XHTML or other formats. This package provides wrapper functions (mainly
 #' [mark()]) based on the \pkg{commonmark} package.
-#' @name markdown
-#' @docType package
-#' @keywords package
-NULL
+'_PACKAGE'
